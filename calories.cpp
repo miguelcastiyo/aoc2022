ca155117290661b7f585b5b3705153b6916a6b67
miguelcastiyo <44275086+miguelcastiyo@users.noreply.github.com>
2022-12-03 16:35:59 -0800

    changed comments

--- calories.cpp
+++ calories.cpp
@@ -4,40 +4,22 @@
 
 using namespace std;
 
-int main(){
+int main() {
 	
-
-	/*
-	while (not eof)
-		add += total
-		if(next line blank)
-			insert add to array(position)
-			reset add to 0
-			increase position tracker
-		
-	-> end while loop
-	find largest item in array(or vector)
-	print it out
-	*/
-	
-	int insertPosition = 0;
 	int totalCalories = 0;
 	int current = 0;
 	vector<int> keepTrack;
 	string input;
 	
 	while(!cin.eof() && getline(cin, input)) {
-//		totalCalories += stoi(input);
 
 		if(!input.empty()) {
 			totalCalories += stoi(input);
 		}
 		 else {
 			keepTrack.push_back(totalCalories);
-                        totalCalories = 0;
+            totalCalories = 0;
 		}
-
-//		cout << "Running total: " << totalCalories << endl;
 	}
 	
 	std::sort(keepTrack.begin(), keepTrack.end());
@@ -47,9 +29,10 @@ int main(){
 		current += keepTrack.back();
 		keepTrack.pop_back();
 	}
+
 	cout << "Largest: " << *max_element(keepTrack.begin(), keepTrack.end()) 
 	     << endl;	
-	cout << "Top 3: " << current << endl;
+	cout << "Top 3 (Added up!): " << current << endl;
 	
 
 	return 0;
